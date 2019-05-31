a4be242c2b428b3a8e97791adb2a0e1b406dda2e
Marco Cesarato <cesarato.developer@gmail.com>
2019-05-31 21:59:23 +0200

    Added composer.json

--- LightSQLParser.php
+++ LightSQLParser.php
@@ -1,12 +1,14 @@
 <?php
 
+namespace marcocesarato\sqlparser;
+
 /**
  * Light SQL Parser Class
  * @author Marco Cesarato <cesarato.developer@gmail.com>
  * @copyright Copyright (c) 2018
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
  * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
- * @version 0.1.91
+ * @version 0.1.95
  */
 class LightSQLParser
 {
